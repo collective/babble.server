8ca559e525758e502bad4006f779d6453d0cab7f
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-02-10 16:02:16 +0000

    Changes to make babble.server zope 2.12 compatible

--- babble/server/browser/service.py
+++ babble/server/browser/service.py
@@ -8,7 +8,7 @@ class ChatServiceAddView:
 
     def __call__(self, add_input_name='', title='', submit_add=''):
         if submit_add:
-            self.context.REQUEST.set('add_input_name', add_input_name)
+            self.request.set('add_input_name', add_input_name)
             obj = ChatService(add_input_name)
             obj.title = title
             obj.manage_addUserFolder()
