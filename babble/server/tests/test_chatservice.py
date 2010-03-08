d38836bcf18772f40a438db32c4c50ff97ad46b7
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-08 21:14:24 +0000

    Rewrote the unit tests and remove unused tests and files. We currently have 100% test coverage. Woohoo

--- babble/server/tests/test_chatservice.py
+++ babble/server/tests/test_chatservice.py
@@ -1,150 +1,335 @@
-import os, sys
+import datetime
+from zope import component
 
-if __name__ == '__main__':
-    execfile(os.path.join(sys.path[0], 'framework.py'))
+from Testing import ZopeTestCase as ztc
+from ZPublisher import NotFound
+from persistent.dict import PersistentDict
 
-from Testing import ZopeTestCase
-import babble.server
+from Products.TemporaryFolder.TemporaryFolder import SimpleTemporaryContainer
 from Products.Five import zcml
-from babble.server.service import ChatService
 
-ZopeTestCase.installProduct('Five')
-ZopeTestCase.installProduct('babble.server')
+import Products.Five
+ztc.installProduct('Five')
+zcml.load_config('configure.zcml', package=Products.Five)
 
+import babble.server
+ztc.installProduct('babble.server')
 zcml.load_config('configure.zcml', package=babble.server)
 
-class TestChatService(ZopeTestCase.ZopeTestCase):
+class TestChatService(ztc.ZopeTestCase):
 
     def afterSetUp(self):
-        '''Adds a babble.server to the default fixture'''
-        self.chatservice = ChatService('chatservice')
-        self.chatservice.manage_addUserFolder()
-        self.chatservice.title = 'Chat Service'
+        """ Adds a babble.server to the default fixture """
+        # Create the admin user.
+        self.setRoles(('Manager',))
 
-    def test_register(self):
+        adding = self.app.restrictedTraverse('+')
+        view = component.getMultiAdapter(
+                                    (adding, self.app.REQUEST),
+                                    name=u'addChatService.html',
+                                    )
+
+        self.assertEqual(type(view()), unicode) # Render the add form
+        view(add_input_name='chat_service', title='Chat Service', submit_add=1)
+
+        # The 'temp_folder' is not created for some reason, so do it here...
+        self.app._setOb('temp_folder', SimpleTemporaryContainer('temp_folder'))
+
+        self.chatservice = self.app.chat_service
+
+
+    def test_get_user(self):
+        """ Test the '_getUser', and 'register' methods
+        """
+        s = self.chatservice
+        self.assertRaises(NotFound, s._getUser, 'username')
+        s.register('username', 'password')
+        self.assertEqual('username', s._getUser('username').id)
+
+
+    def test_get_users_folder(self):
+        """ Test the '_getUsersFolder' method
+        """ 
+        s = self.chatservice
+        f = s._getUsersFolder()
+        self.assertEqual(f, s.users)
+        
+        s.manage_delObjects(['users'])
+        f = s._getUsersFolder()
+        self.assertEqual(f, s.users)
+
+
+    def test_user_access_dict(self):
+        """ Test the '_getUserAccessDict' method
+        """ 
+        s = self.chatservice
+        uad = s._getUserAccessDict()
+        self.assertEqual(uad, PersistentDict())
+
+        # Test that the 'user access dict' is recreated if it is deleted (which
+        # is plausible since it's in a temp folder)
+        s.temp_folder._getOb('user_access_dict')
+        uad = s._getUserAccessDict()
+        self.assertEqual(uad, PersistentDict())
+
+        # Test the NotFound is raised when the 'temp_folder' is not there
+        self.app._delOb('temp_folder')
+        self.assertRaises(NotFound, s._getUserAccessDict)
+
+
+    def test_registration(self):
+        """ Test the 'register', 'isRegistered', 'authenticate' and
+            'setUserPassword'  methods.
+        """
+        s = self.chatservice
+        s.register('username', 'password')
+        self.assertEqual('username', s._getUser('username').id)
+
+        r = s.isRegistered('username')
+        self.assertEqual(r, True)
+
+        auth = s.authenticate('username', 'password')
+        self.assertEqual(auth != None, True)
+        self.assertEqual(auth.name, 'username')
+
+        r = s.isRegistered('nobody')
+        self.assertEqual(r, False)
+
+        auth = s.authenticate('nobody', 'password')
+        self.assertEqual(auth, None)
+
+        s.setUserPassword('username', 'new_password')
+        auth = s.authenticate('username', 'password')
+        self.assertEqual(auth, None)
+
+        auth = s.authenticate('username', 'new_password')
+        self.assertEqual(auth != None, True)
+        self.assertEqual(auth.name, 'username')
+
+
+    def test_online(self):
+        """ Test the '_confirmAsOnline', 'isOnline' and 'getOnlineUsers' methods """
         s = self.chatservice
         u = 'username'
         s.register(u, u)
-        self.assertEqual(u, s._getUser(u).id)
-
-    def test_setUserProperty(self):
-        s = self.chatservice
-        u = 'username'
-        s.register(u, u)
-        s.manage_addProperty('name', '', 'string')
-        self.assertEqual(s.setUserProperty(u, 'name', 'pete'), None)
-
-    def test_getUserProperty(self):
-        s = self.chatservice
-        u = 'username'
-        self.test_setUserProperty()
-        self.assertEqual(s.getUserProperty(u, 'name'), 'pete')
-
-    def test_findUser(self):
-        s = self.chatservice
-        u = 'username'
-        self.test_setUserProperty()
-        self.assertEqual(s.findUser('name', 'pete'), ['username'])
-
-    def test_signIn(self):
-        s = self.chatservice
-        u = 'username'
-        s.register(u, u)
-        self.assertEqual(s.signIn(u), None)
-
-    def test_signOut(self):
-        s = self.chatservice
-        u = 'username'
-        s.register(u, u)
-        self.assertEqual(s.signOut(u), None)
-
-    def test_isOnline(self):
-        """ check if user is online """
-        s = self.chatservice
-        u = 'username'
-        s.register(u, u)
-        s.signIn(u)
-        self.assertEqual(s.isOnline(u), True)
-        s.signOut(u)
         self.assertEqual(s.isOnline(u), False)
 
-    def test_requestContact(self):
-        """ make contact request """
-        s = self.chatservice
-        s.register('chatterbox', 'passwd')
-        s.register('buddy', 'passwd')
-        s.requestContact('chatterbox', 'buddy')
-        self.assertEqual(s.getContactRequests('chatterbox'), ('buddy',))
-        self.assertEqual(s.getPendingContacts('buddy'), ('chatterbox',))
+        # Test that a user entry was made into the 'user access dict'
+        s._confirmAsOnline(u)
+        uad = s._getUserAccessDict()
+        self.assertEqual(uad.get(u, None) != None, True)
 
-    def test_approveContactRequest(self):
-        """ make contact request """
-        s = self.chatservice
-        s.register('chatterbox', 'passwd')
-        s.register('buddy', 'passwd')
-        s.requestContact('chatterbox', 'buddy')
-        s.approveContactRequest('buddy', 'chatterbox')
-        self.assertEqual(s.getContactRequests('chatterbox'), ())
-        self.assertEqual(s.getPendingContacts('buddy'), ())
-        self.assertEqual(s.getContacts('buddy'), ('chatterbox',))
-        self.assertEqual(s.getContacts('chatterbox'), ('buddy',))
+        self.assertEqual(s.isOnline(u), True)
 
-    def test_declineContactRequest(self):
-        """ make contact request """
-        s = self.chatservice
-        s.register('chatterbox', 'passwd')
-        s.register('buddy', 'passwd')
-        s.requestContact('chatterbox', 'buddy')
-        s.declineContactRequest('buddy', 'chatterbox')
-        self.assertEqual(s.getContactRequests('chatterbox'), ())
-        self.assertEqual(s.getPendingContacts('buddy'), ())
-        self.assertEqual(s.getContacts('buddy'), ())
-        self.assertEqual(s.getContacts('chatterbox'), ())
+        now = datetime.datetime.now()
+        
+        # Test that a user that was confirmed as online 59 seconds ago (i.e
+        # less than a minute) is still considered as online.
+        delta = datetime.timedelta(seconds=59)
+        uad[u] = datetime.datetime.now() - delta
+        self.assertEqual(s.isOnline(u), True)
 
-    def test_sendMessage(self):
-        """ send a message to a user """
+        ou = s.getOnlineUsers()
+        self.assertEquals(ou, [u])
+
+        # Test that a user that was confirmed as online one minute ago (i.e
+        # at least a minute) is now considered as offline.
+        delta = datetime.timedelta(minutes=1)
+        uad[u] = datetime.datetime.now() - delta
+        self.assertEqual(s.isOnline(u), False)
+
+        ou = s.getOnlineUsers()
+        self.assertEquals(ou, [])
+
+        # Test 'getOnlineUsers' with multiple online users.
+        s._confirmAsOnline(u)
+
+        u = 'another'
+        s.register(u, u)
+        s._confirmAsOnline(u)
+
+        ou = s.getOnlineUsers()
+        self.assertEquals(ou, ['username', 'another'])
+
+
+    def test_status(self):
+        """ Test the 'setStatus' and 'getStatus' methods
+        """
         s = self.chatservice
         u = 'username'
         s.register(u, u)
-        s.register('recipient', 'passwd')
-        self.assertEqual(s.sendMessage(u, 'recipient', 'message'), None)
 
-    def test_getMessagesForUser(self):
-        """ get all the messages for a user from a given sender"""
-        s = self.chatservice
-        u = 'username'
-        s.register(u, u)
-        s.register('recipient', 'passwd')
-        self.assertEqual(s.getMessagesForUser(u, 'recipient'), ())
-        s.sendMessage(u, 'recipient', 'message')
-        self.assertEqual(s.getMessagesForUser(u, 'recipient'), ())
+        self.assertEqual(s.getStatus(u), 'offline')
 
-    def test_getContactStatusList(self):
-        """ test contact status list """
+        s._confirmAsOnline(u)
+        self.assertEqual(s.getStatus(u), 'online')
+
+        s.setStatus(u, 'busy')
+        self.assertEqual(s.getStatus(u), 'busy')
+
+        s.setStatus(u, 'away')
+        self.assertEqual(s.getStatus(u), 'away')
+
+        # Simulate one minute of time passing and then test the that user's
+        # status is 'offline'
+        delta = datetime.timedelta(minutes=1)
+        uad = s._getUserAccessDict()
+        uad[u] = datetime.datetime.now() - delta
+        self.assertEqual(s.getStatus(u), 'offline')
+
+
+    def test_messaging(self):
+        """ Test the 'sendMessage' and 'getUnreadMessages' methods """
         s = self.chatservice
-        s.register('chatterbox', 'passwd')
-        s.register('buddy', 'passwd')
-        s.requestContact('chatterbox', 'buddy')
-        self.assertEqual(s.getContactStatusList('chatterbox'),
-            [('buddy', 'requesting', 0)])
-        self.assertEqual(s.getContactStatusList('buddy'),
-            [('chatterbox', 'pending', 0)])
-        s.approveContactRequest('buddy', 'chatterbox')
-        self.assertEqual(s.getContactStatusList('buddy'),
-            [('chatterbox', 'offline', 0)])
-        self.assertEqual(s.getContactStatusList('chatterbox'),
-            [('buddy', 'offline', 0)])
-        s.sendMessage('chatterbox', 'buddy', 'hallo')
-        self.assertEqual(s.getContactStatusList('chatterbox'),
-            [('buddy', 'offline', 0)])
-        self.assertEqual(s.getContactStatusList('buddy'),
-            [('chatterbox', 'offline', 1)])
+        s.register('sender', 'sender')
+        s.register('recipient', 'passwd')
+
+        um = s.getUnreadMessages(
+                                'recipient',
+                                sender='sender',
+                                read=True,
+                                confirm_online=True,
+                                )
+        self.assertEqual(um, [])
+        s.sendMessage('sender', 'recipient', 'message')
+
+        um = s.getUnreadMessages(
+                                'recipient',
+                                sender='sender',
+                                read=False,
+                                confirm_online=True,
+                                )
+        # The unread messages datastructure looks as follows:
+        # [
+        #   {
+        #    'messages': (('username', '2010/03/08', '15:25', 'message'),), 
+        #    'user': 'username'
+        #    }
+        # ]
+        #
+        # Test that messages from only one user was returned
+        self.assertEqual(len(um), 1)
+        # Test that only one message was received from this user
+        self.assertEqual(len(um[0]['messages']), 1)
+        # Test that the message tuple has 4 elements
+        self.assertEqual(len(um[0]['messages'][0]), 4)
+        # Test that senders username
+        self.assertEqual(um[0]['messages'][0][0], 'sender')
+        # Test that message date
+        self.assertEqual(um[0]['messages'][0][1], 
+                    datetime.datetime.now().strftime("%Y/%m/%d"))
+        # Test that message time
+        self.assertEqual(um[0]['messages'][0][2], 
+                    datetime.datetime.now().strftime("%H:%M"))
+        # Test that message text
+        self.assertEqual(um[0]['messages'][0][3], 'message')
+
+
+        # Test getUnreadMessages with multiple senders. We didn't mark the
+        # previous sender's messages as 'read', so they should be returned
+        # again.
+        s.register('sender2', 'sender2')
+        s.sendMessage('sender2', 'recipient', 'another message')
+        um = s.getUnreadMessages(
+                                'recipient',
+                                read=True,
+                                confirm_online=True,
+                                )
+        # Test that messages from two users were returned
+        self.assertEqual(len(um), 2)
+        # Test that only one message was received from each
+        self.assertEqual(len(um[0]['messages']), 1)
+        self.assertEqual(len(um[1]['messages']), 1)
+
+        # Test the properties of the message sent by sender1
+        self.assertEqual(len(um[1]['messages'][0]), 4)
+        self.assertEqual(um[1]['messages'][0][0], 'sender2')
+        self.assertEqual(um[1]['messages'][0][1], 
+                    datetime.datetime.now().strftime("%Y/%m/%d"))
+        self.assertEqual(um[1]['messages'][0][2], 
+                    datetime.datetime.now().strftime("%H:%M"))
+        self.assertEqual(um[1]['messages'][0][3], 'another message')
+
+        # All the unread messages for 'recipient' has now been marked as read,
+        # lets test that no new messages are returned
+        um = s.getUnreadMessages('recipient')
+        self.assertEqual(um, [])
+
+
+    def test_message_clearing(self):
+        """ Test the 'sendMessage' and 'getUnclearedMessages' methods """
+        s = self.chatservice
+        s.register('sender', 'sender')
+        s.register('recipient', 'passwd')
+
+        um = s.getUnclearedMessages('recipient', sender='sender')
+        self.assertEqual(um, [])
+        s.sendMessage('sender', 'recipient', 'message')
+
+        um = s.getUnclearedMessages(
+                                'recipient',
+                                sender='sender',
+                                clear=False,
+                                confirm_online=True,)
+        # The uncleared messages datastructure looks as follows:
+        # [
+        #   {
+        #    'messages': (('username', '2010/03/08', '15:25', 'message'),), 
+        #    'user': 'username'
+        #    }
+        # ]
+        #
+        # Test that messages from only one user was returned
+        self.assertEqual(len(um), 1)
+        # Test that only one message was received from this user
+        self.assertEqual(len(um[0]['messages']), 1)
+        # Test that the message tuple has 4 elements
+        self.assertEqual(len(um[0]['messages'][0]), 4)
+        # Test that senders username
+        self.assertEqual(um[0]['messages'][0][0], 'sender')
+        # Test that message date
+        self.assertEqual(um[0]['messages'][0][1], 
+                    datetime.datetime.now().strftime("%Y/%m/%d"))
+        # Test that message time
+        self.assertEqual(um[0]['messages'][0][2], 
+                    datetime.datetime.now().strftime("%H:%M"))
+        # Test that message text
+        self.assertEqual(um[0]['messages'][0][3], 'message')
+
+
+        # Test getUnclearedMessages with multiple senders. We didn't mark the
+        # previous sender's messages as 'clear', so they should be returned
+        # again.
+        s.register('sender2', 'sender2')
+        s.sendMessage('sender2', 'recipient', 'another message')
+        um = s.getUnclearedMessages(
+                                'recipient',
+                                clear=True,
+                                confirm_online=True,
+                                )
+        # Test that messages from two users were returned
+        self.assertEqual(len(um), 2)
+        # Test that only one message was received from each
+        self.assertEqual(len(um[0]['messages']), 1)
+        self.assertEqual(len(um[1]['messages']), 1)
+
+        # Test the properties of the message sent by sender1
+        self.assertEqual(len(um[1]['messages'][0]), 4)
+        self.assertEqual(um[1]['messages'][0][0], 'sender2')
+        self.assertEqual(um[1]['messages'][0][1], 
+                    datetime.datetime.now().strftime("%Y/%m/%d"))
+        self.assertEqual(um[1]['messages'][0][2], 
+                    datetime.datetime.now().strftime("%H:%M"))
+        self.assertEqual(um[1]['messages'][0][3], 'another message')
+
+        # All the uncleared messages for 'recipient' has now been marked as clear,
+        # lets test that no new messages are returned
+        um = s.getUnclearedMessages('recipient')
+        self.assertEqual(um, [])
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestChatService))
     return suite
-
-if __name__ == '__main__':
-    framework()
