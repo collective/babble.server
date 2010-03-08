c8a45e1499a82cafd5be414a85e84a7ed298951f
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-08 21:11:10 +0000

    Some refactoring. Removed unused methods, added docstrings.

--- babble/server/service.py
+++ babble/server/service.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime
+import datetime
 
 from zope.interface import implements
 
@@ -7,11 +7,9 @@ from AccessControl import ClassSecurityInfo
 from Globals import InitializeClass
 from OFS.Folder import Folder
 from ZPublisher import NotFound
-from zExceptions import BadRequest
 from persistent.dict import PersistentDict
 
 from Products.BTreeFolder2.BTreeFolder2 import manage_addBTreeFolder
-from Products.TemporaryFolder.TemporaryFolder import constructTemporaryFolder 
 
 from interfaces import IChatService
 from user import User
@@ -19,112 +17,93 @@ from user import User
 log = logging.getLogger('babble.server/service.py')
 
 class ChatService(Folder):
-    """ Chat Service """
+    """ """
     implements(IChatService)
     security = ClassSecurityInfo()
     security.declareObjectProtected('Use Chat Service')
 
     def _getUserAccessDict(self):
         """ The user access dictionary is stored inside a Temporary Folder.
-            The contents of a Temporary Folder is kept in RAM and loses all
-            it's contents whenever Zope is restarted.
+            A Temporary Folder is kept in RAM and it loses all its contents 
+            whenever the Zope server is restarted.
 
-            The user access dictionary contains usernames as keys and the
+            The 'user access dictionary' contains usernames as keys and the
             last date and time that these users have been confirmed to be 
             online as the values.
 
             These date values can be used to determine (guess) whether the 
             user is still currently online.
         """
-        if not self.hasObject('temp_folder'):
+        if not hasattr(self, 'temp_folder'): # Acquisition
             log.warn("The chatservice 'Online Users' folder did not exist, "
                 "and has been automatically recreated.")
+            raise NotFound("/temp_folder does not exist.")
 
-            constructTemporaryFolder(self, 'temp_folder')
             
-        temp_folder = self._getOb('temp_folder')
+        temp_folder = self.temp_folder # Acquisition
         if not temp_folder.hasObject('user_access_dict'):
             log.info("The user_access_dict did not exist, "
-                "and has been automatically recreated.")
-
+                    "and has been automatically recreated.")
             temp_folder._setOb('user_access_dict', PersistentDict())
 
         return temp_folder._getOb('user_access_dict')
 
+
     def _getUsersFolder(self):
+        """ The 'Users' folder is a BTreeFolder that contains IUser objects.
+            See babble.server.interfaces.py:IUser
+        """
         if not self.hasObject('users'):
             log.warn("The chatservice 'Users' folder did not exist, "
-                "and has been automatically recreated.")
-
+                    "and has been automatically recreated.")
             manage_addBTreeFolder(self, 'users', 'Users')
-            
+
         return self._getOb('users')
 
-    def _getUser(self, username, auto_register=True):
+
+    def _getUser(self, username):
+        """ Retrieve the IUser obj from the 'Users' folder.
+        """
         users = self._getUsersFolder()
         if not users.hasObject(username):
-            if auto_register:
-                self.register(username, username[::-2]*2)
-            else:
-                raise NotFound("%s is not registered." % username)
-
+            raise NotFound("%s is not registered." % username)
         return users._getOb(username)
 
+
+    def _confirmAsOnline(self, username):
+        """ Confirm that the user is currently online by updating the 'user
+            access dict'
+        """
+        uad = self._getUserAccessDict()
+        uad[username] = datetime.datetime.now()
+
+
     def register(self, username, password):
-        """ register a user with the babble.server """
+        """ Register a user with the babble.server's acl_users and create a
+            'User' object in the 'Users' folder
+        """
         self.acl_users.userFolderAddUser(
                         username, password, roles=(), domains=())
 
         users = self._getUsersFolder()
         users._setObject(username, User(username))
 
+
     def isRegistered(self, username):
-        """ Check whether the user is registered """
-        if self.users.hasObject(username):
-            return True
-        return False
+        """ Check whether the user is registered via acl_users """
+        return self.acl_users.getUser(username) and True or False
+
 
     def setUserPassword(self, username, password):
-        """ set user's password """
+        """ Set the user's password """
         self.acl_users.userFolderEditUser(
                     username, password, roles=(), domains=())
 
-    def setUserProperty(self, username, propname, value):
-        """ set a property for a given user """
-        if not self.hasProperty(propname):
-            raise BadRequest, 'Undefined property'
 
-        user = self._getUser(username)
-
-        proptype = self.getPropertyType(propname)
-        if not user.hasProperty(propname):
-            user.manage_addProperty(propname, value, proptype)
-        else:
-            props = {propname: value}
-            user.manage_changeProperties(props)
-
-    def getUserProperty(self, username, propname):
-        """ get the property for a given user """
-        if not self.hasProperty(propname):
-            raise BadRequest, 'Undefined property'
-
-        user = self._getUser(username)
-        return user.getProperty(propname)
-
-    def findUser(self, propname, value):
-        """ Find a user on a value for a given property """
-        result = []
-        for user in self.objectValues():
-            if user.getProperty(propname) == value:
-                result.append(user.getId())
-        return result
-
-    def confirmAsOnline(self, username):
-        """ Confirm that the user is currently online by updating the 'user
-            access dict'
-        """
-        uad = self._getUserAccessDict()
-        uad[username] = datetime.now()
+    def authenticate(self, username, password):
+        """ Authenticate the user with username and password """
+        return self.acl_users.authenticate(username, password, self.REQUEST)
+        
 
     def isOnline(self, username):
         """ Determine whether the user is (probably) currently online
@@ -135,34 +114,26 @@ class ChatService(Folder):
             If yes, then we assume the user is online, otherwise not.
         """
         uad = self._getUserAccessDict()
-        last_confirmed_date = uad.get(username, datetime.min)
-        now = datetime.now()
-        minute = now.minute  or 1
-        cutoff_date = now.replace(minute=minute-1)
+        last_confirmed_date = uad.get(username, datetime.datetime.min)
+        delta = datetime.timedelta(minutes=1)
+        cutoff_date = datetime.datetime.now() - delta
         return last_confirmed_date > cutoff_date
+
 
     def getOnlineUsers(self):
         """ Determine the (probable) online users from the 'user access dict' 
             and return them as a list
         """
-        online_users = []
         uad = self._getUserAccessDict()
-        for username in uad.keys():
-            last_confirmed_date = uad.get(username, datetime.min)
-            now = datetime.now()
-            minute = now.minute  or 1
-            cutoff_date = now.replace(minute=minute-1)
-            if last_confirmed_date > cutoff_date:
-                online_users.append(username)
+        return [user for user in uad.keys() if self.isOnline(user)]
 
-        return online_users
 
     def setStatus(self, username, status):
         """ Set the user's status.
 
             The user might have a status such as 'available', 'chatty', 
             'busy' etc. but this only applies if the user is actually 
-            online.
+            online, as determined from the 'user access dictionary'.
 
             The 'status' attribute is optional, it depends on the chat client 
             whether the user's 'status' property is at all relevant 
@@ -171,12 +142,13 @@ class ChatService(Folder):
         user = self._getUser(username)
         user.setStatus(status)
 
+
     def getStatus(self, username):
         """ Get the user's status.
 
             The user might have a status such as 'available', 'chatty', 
             'busy' etc. but this only applies if the user is actually 
-            online.
+            online, as determined from the 'user access dictionary'.
 
             The 'status' attribute is optional, it depends on the chat client 
             whether the user's 'status' property is at all relevant 
@@ -188,44 +160,45 @@ class ChatService(Folder):
         user = self._getUser(username)
         return user.getStatus()
 
+
     def sendMessage(self, sender, recipient, message, register=False):
-        """ Send a message to a user 
+        """ Sends a message to recipient
 
             A message is added to the messagebox of both the sender and
             recipient.
         """
-        # Add for the sender but make sure it's set to read.
-        user = self._getUser(sender, register)
+        # Add for the sender, but make sure it's set to read.
+        user = self._getUser(sender)
         user.addMessage(recipient, message, sender, read=True)
 
-        user = self._getUser(recipient, register)
+        user = self._getUser(recipient)
         user.addMessage(sender, message, sender)
+
 
     def getUnreadMessages(
                         self, 
                         username, 
                         sender=None, 
-                        register=True,
                         read=True,
                         confirm_online=True,
                         ):
-        """ Returns the unread messages for user. 
+        """ Returns the unread messages for a user. 
             
             If sender is none, return all unread messages, otherwise return
-            only the unread messages for that specific sender.
+            only the unread messages sent by that specific sender.
         """
         if confirm_online:
-            self.confirmAsOnline(username)
+            self._confirmAsOnline(username)
 
-        user = self._getUser(username, register)
+        user = self._getUser(username)
         messages = user.getUnreadMessages(sender, read)
         return messages
+
 
     def getUnclearedMessages(
                         self, 
                         username, 
                         sender=None, 
-                        register=True,
                         read=True,
                         clear=False,
                         confirm_online=True,
@@ -233,64 +206,15 @@ class ChatService(Folder):
         """ Returns the uncleared messages for user. 
             
             If sender is none, return all uncleared messages, otherwise return
-            only the uncleared messages for that specific sender.
+            only the uncleared messages sent by that specific sender.
         """
         if confirm_online:
-            self.confirmAsOnline(username)
+            self._confirmAsOnline(username)
 
-        user = self._getUser(username, register)
+        user = self._getUser(username)
         messages = user.getUnclearedMessages(sender, read, clear)
         return messages
 
-    def requestContact(self, username, contact):
-        """ make request to add contact for a given user """
-        user = self._getUser(username)
-        user.requestContact(contact)
-        user = self._getUser(contact)
-        user.addPendingContact(username)
-
-    def approveContactRequest(self, username, contact):
-        """ approve a contact request """
-        user = self._getUser(username)
-        user.approveContactRequest(contact)
-        user = self._getUser(contact)
-        user.approveContactRequest(username)
-
-    def declineContactRequest(self, username, contact):
-        """ decline request to add user as contact """
-        user = self._getUser(username)
-        user.declineContactRequest(contact)
-        user = self._getUser(contact)
-        user.declineContactRequest(username)
-
-    def removeContact(self, username, contact):
-        """ remove existing contact """
-        user = self._getUser(username)
-        user.removeContact(contact)
-        user = self._getUser(contact)
-        user.removeContact(username)
-
-    def getPendingContacts(self, username):
-        """ return a list of of all pending contacts """
-        user = self._getUser(username)
-        return user.getPendingContacts()
-
-    def getContactRequests(self, username):
-        """ return a list of of all requests to be add as contact for a
-            given user
-        """
-        user = self._getUser(username)
-        return user.getContactRequests()
-
-    def getContacts(self, username):
-        """ return a list of all contacts """
-        user = self._getUser(username)
-        return user.getContacts()
-
-    def getContactStatusList(self, username):
-        """ return a more detailed list of all contacts """
-        user = self._getUser(username)
-        return user.getContactStatusList()
 
 InitializeClass(ChatService)
 
