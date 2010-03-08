87702c4db8152e82e1de1c3052ca27fd155dd78a
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-08 21:13:11 +0000

    Cleaned up and added docstrings

--- babble/server/messagebox.py
+++ babble/server/messagebox.py
@@ -4,10 +4,10 @@ from message import Message
 from interfaces import IMessageBox
 
 class MessageBox(BTreeFolder2):
-    """ A message box """
+    """ A container for messages """
     implements(IMessageBox)
 
     def addMessage(self, message, author, read=False):
-        """ add a message """
+        """ Add a message to the MessageBox """
         message = Message(message, author, read)
         self._setObject(message.id, message)
