913ac7dc0e3cb8a0d68cc76adec6867d684f9e0c
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-10-18 06:36:06 +0000

    Finish the upgrade script and rename it to avoid the dot.

---
+++ babble/server/Extensions/upgrade_to_1_0.py
@@ -0,0 +1,71 @@
+import time
+import dateutil.parser
+import transaction
+from pytz import utc
+from zExceptions import BadRequest
+from DateTime.interfaces import IDateTime
+from Products.BTreeFolder2.BTreeFolder2 import manage_addBTreeFolder
+from babble.server.conversation import Conversation
+from babble.server.messagebox import MessageBox
+from babble.server.interfaces import IChatService
+from babble.server.message import Message
+
+def run(self):
+    """
+    * Move existing messages from messageboxes in IUser objects, messageboxes
+      in IConversation objects.
+    * Rename all old messages' ids to remove the previx "message."
+    * Change all old messages' time attr to python's datetime.
+    """
+    services = []
+    for o in self.objectValues():
+        if IChatService.providedBy(o):
+            services.append(o)
+
+    for service in services:
+
+        if not hasattr(service, 'conversations'):
+            manage_addBTreeFolder(service, 'conversations', 'Conversations')
+        convs = service._getOb('conversations')
+
+        for user in getattr(service, 'users').objectValues():
+            for oldmbox in user.objectValues():
+
+                ord1 = ''.join([str(ord(c)) for c in user.id])
+                ord2 = ''.join([str(ord(c)) for c in oldmbox.id])
+                cid = '-'.join(sorted([ord1, ord2]))
+                if not convs.hasObject(cid):
+                    convs._setObject(cid, Conversation(cid, user.id, oldmbox.id))
+                conv = convs._getOb(cid)
+                if oldmbox.id not in conv.objectIds():
+                    conv._setObject(oldmbox.id, MessageBox(oldmbox.id))
+                newmbox = conv._getOb(oldmbox.id)
+                mids = []
+                oldids = []
+                newids = []
+                for message in oldmbox.objectValues():
+                    if message.author != oldmbox.id:
+                        # We now only store one author's messages per mbox. So
+                        # we ignore the other messages. They will be deleted.
+                        continue
+
+                    if IDateTime.providedBy(message.time):
+                        message.time = message.time.asdatetime().replace(tzinfo=utc)
+                    elif type(message.time) == str:
+                        message.time = dateutil.parser.parse(message.time)
+
+                    newid = '%f' % time.mktime(message.time.timetuple())
+                    message.time = message.time.isoformat()
+
+                    newmsg= Message(message.text, message.author)
+                    newmsg.time = message.time
+                    newmsg.id = newid
+                    try:
+                        newmbox._setObject(newid, message)
+                    except BadRequest:
+                        continue
+
+                transaction.commit()
+
+    return "Succesfully migrated the messages"
+
