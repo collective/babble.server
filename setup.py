97bb759245630acdde022e933a389360511395ae
jcbrand <jcbrand@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2012-03-23 17:09:58 +0000

    Back to development: 1.0b6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b5'
+version = '1.0b6.dev0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
