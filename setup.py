545034bf3c11b655b411fe84fa349d921fb3d634
JC Brand <jc@opkode.com>
2012-04-10 18:13:31 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.1.dev0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
